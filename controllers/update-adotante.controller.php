5836decbe216618777ae20cd0b393dab450ade2e
Junior <juniorod99@gmail.com>
2025-03-20 20:10:11 -0300

    Alterando documentos do adontante e excluindo o antigo.

--- controllers/update-adotante.controller.php
+++ controllers/update-adotante.controller.php
@@ -26,8 +26,24 @@ foreach ($dadosFormulario as $campo => $valorForm) {
     }
 }
 
+if (!empty($doc['name'])) {
+    echo "enviou documento";
+    $novoNomeDoc = md5(rand());
+    $extensaoDoc = pathinfo($_FILES['documentos']['name'], PATHINFO_EXTENSION);
+    $documentos = "adotantes-docs/$novoNomeDoc.$extensaoDoc";
+    $camposAlterados[] = "documentos = :documentos";
+    $params[] = $documentos;
+    move_uploaded_file($_FILES['documentos']['tmp_name'], __DIR__ . '/../assets/img/' . $documentos);
+    if ($dadosAdotante->documentos) {
+        if (file_exists(__DIR__ . '/../assets/img/' . $dadosAdotante->documentos)) {
+            unlink(__DIR__ . '/../assets/img/' . $dadosAdotante->documentos);
+        }
+    }
+}
+
+// dd($camposAlterados, $params, $doc);
+
 if (!empty($img['name'])) {
-    echo "enviou foto <br>";
     $novoNome = md5(rand());
     $extensao = pathinfo($_FILES['imagem']['name'], PATHINFO_EXTENSION);
     $imagem = "adotantes/$novoNome.$extensao";
@@ -35,9 +51,7 @@ if (!empty($img['name'])) {
     $params[] = $imagem;
     move_uploaded_file($_FILES['imagem']['tmp_name'], __DIR__ . '/../assets/img/' . $imagem);
     if ($dadosAdotante->foto) {
-        echo "ele ja tinha foto <br>";
         if (file_exists(__DIR__ . '/../assets/img/' . $dadosAdotante->foto)) {
-            echo "arquivo existe";
             unlink(__DIR__ . '/../assets/img/' . $dadosAdotante->foto);
         }
     }
@@ -45,12 +59,10 @@ if (!empty($img['name'])) {
 
 $params[] = $id;
 
-echo "<br>update adotantes set "  . implode(', ', $camposAlterados) . " where id = :id";
-// dd($camposAlterados, $params);
 $database->query(
     "update adotantes set " . implode(', ', $camposAlterados) . " where id = ?",
     params: $params
 );
 
-header('Location: /home');
+header('Location: /adotantes');
 exit();
